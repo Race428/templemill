bfe2f5909c08106a44255f7b9c9edc68813f7edf
Race428 <raceacheson@gmail.com>
2019-07-29 10:32:41 -0600

    css on video grid

--- src/components/pages/work.js
+++ src/components/pages/work.js
@@ -8,6 +8,8 @@ import '../../css/styles.css'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import $ from 'jquery'
+import Iframe from 'react-iframe'
+
 
 
 
@@ -28,22 +30,127 @@ class Work extends Component {
 
 
 
+    iframePlay = () => {
+
+
+        $('#album-cover-image, #play-icon').fadeOut('fast')
+        $('#iframe-hidden').attr('id', 'iframe-shown')
+
+
+
+
+    }
+
     render() {
 
 
         return (
 
             <>
-            <div id='showcase'>
+                <div id='showcase'>
                     <header className='nav-container'>
-                        <Nav props={this.props}/>
+                        <Nav props={this.props} />
                     </header>
 
                     <div id='work'>
                         <h2> A blast from the past to inspire the now.</h2>
-                    </div>
+
+
+                        {/* album   */}
+
+                        <div id='video-album'>
+
+                            <div id='album-photo-cover-button-container'>
+
+                                <div className='album-button-video' onClick={this.iframePlay}>
+                                    <img id='album-cover-image' src='https://static1.squarespace.com/static/5aa301a9b27e39ca4eddba98/5ba2fedf4ae237797f72ba56/5ba3017f0ebbe87a884fdebc/1537899852197/SizzleTHumbnail.JPG?format=2500w' alt='pic' />
+                                    <img id='play-icon' src='https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c4f7.png' alt='play-icon' />
+                                    <div id="iframe-hidden"
+                                        className="album-iframe" >
+                                        <Iframe url="https://player.vimeo.com/video/289365725?autoplay=1&loop=1&autopause=0"
+
+                                            allow='autoplay'
+                                            display="initial"
+
+                                            allowFullScreen />
+                                    </div>
+
+
+                                </div>
+                            </div>
+
+                            <div id='album-photo-cover-button-container'>
+
+                                <div className='album-button-video' onClick={this.iframePlay}>
+                                    <img id='album-cover-image' src='https://static1.squarespace.com/static/5aa301a9b27e39ca4eddba98/5ba2fedf4ae237797f72ba56/5ba3017f0ebbe87a884fdebc/1537899852197/SizzleTHumbnail.JPG?format=2500w' alt='pic' />
+                                    <img id='play-icon' src='https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c4f7.png' alt='play-icon' />
+                                    <div id="iframe-hidden"
+                                        className="album-iframe" >
+                                        <Iframe url="https://player.vimeo.com/video/289365725?autoplay=1&loop=1&autopause=0"
+
+                                            allow='autoplay'
+                                            display="initial"
+
+                                            allowFullScreen />
+                                    </div>
+
+
+                                </div>
+                            </div>
+
+                            <div id='album-photo-cover-button-container'>
+
+                                <div className='album-button-video' onClick={this.iframePlay}>
+                                    <img id='album-cover-image' src='https://static1.squarespace.com/static/5aa301a9b27e39ca4eddba98/5ba2fedf4ae237797f72ba56/5ba3017f0ebbe87a884fdebc/1537899852197/SizzleTHumbnail.JPG?format=2500w' alt='pic' />
+                                    <img id='play-icon' src='https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c4f7.png' alt='play-icon' />
+                                    <div id="iframe-hidden"
+                                        className="album-iframe" >
+                                        <Iframe url="https://player.vimeo.com/video/289365725?autoplay=1&loop=1&autopause=0"
+
+                                            allow='autoplay'
+                                            display="initial"
+
+                                            allowFullScreen />
+                                    </div>
+
+
+                                </div>
+                            </div>
+
+
+                            <div id='album-photo-cover-button-container'>
+
+                                <div className='album-button-video' onClick={this.iframePlay}>
+                                    <img id='album-cover-image' src='https://static1.squarespace.com/static/5aa301a9b27e39ca4eddba98/5ba2fedf4ae237797f72ba56/5ba3017f0ebbe87a884fdebc/1537899852197/SizzleTHumbnail.JPG?format=2500w' alt='pic' />
+                                    <img id='play-icon' src='https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c4f7.png' alt='play-icon' />
+                                    <div id="iframe-hidden"
+                                        className="album-iframe" >
+                                        <Iframe url="https://player.vimeo.com/video/289365725?autoplay=1&loop=1&autopause=0"
+
+                                            allow='autoplay'
+                                            display="initial"
+
+                                            allowFullScreen />
+                                    </div>
+
+
+                                </div>
+                            </div>
+
+
+
+
+
+
+
+                        </div>
+
+
+                        <div id='horizontal-line-2'></div>
+
 
                     </div>
+                </div>
 
             </>
 
